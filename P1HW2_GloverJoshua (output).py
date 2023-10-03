aa454d6a991493d9eff4a82c81ea2ff6f6aa6060
gloverj4079 <145460523+gloverj4079@users.noreply.github.com>
2023-10-03 17:59:01 -0400

    Add files via upload

---
+++ P1HW2_GloverJoshua (output).py
@@ -0,0 +1,18 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Glover/OneDrive/Desktop/P1HW2.py
+This program calculates and displays travel expense
+Enter Budget: 2000
+Enter your travel destination: Atlanta
+How much do you think you will spend on gas? 400
+Approxiamatley, hohw much will you have for accommodation/hotel? 1000
+Last, hoow much do you need for food? 400
+----------Travel Expense----------
+Location: Atlanta
+Initial Budget 2000
+
+fuel: 400
+Accommodation: 1000
+Food: 400
+Remaining Balance: 200
